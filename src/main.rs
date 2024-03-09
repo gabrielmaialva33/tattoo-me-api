5a052e9d68fdfca4ffb6f8872c262be3fd518171
Gabriel Maia <gabrielmaialva33@gmail.com>
2024-03-09 00:28:24 -0300

    feat: init actix server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(index))
+    })
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
+}
+
+
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
 }
